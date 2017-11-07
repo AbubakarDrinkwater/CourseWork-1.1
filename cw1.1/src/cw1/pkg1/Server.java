d9c92ff9a8b61fde76ac418cdfd287e70e5ff9ab
M00561037 <M00561037@kvmdxlab110pc11.studentmdx.ac>
2017-11-07 15:33:28 +0400

    My first commit

---
+++ cw1.1/src/cw1/pkg1/Server.java
@@ -0,0 +1,17 @@
+package cw1.pkg1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */import java.util.Scanner;
+package coursework1;
+
+/**
+ *
+ * @author M00561037
+ */
+public class Server {
+    
+    System.out.println("")
+}
